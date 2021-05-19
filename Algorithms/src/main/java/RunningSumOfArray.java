e7a3221af738f8b1e1536dadf0f91ee9a73eb02c
K-Bhanu-Prasad <83743790+K-Bhanu-Prasad@users.noreply.github.com>
2021-05-19 23:48:16 +0530

    Added program Running sum of array

---
+++ Algorithms/src/main/java/RunningSumOfArray.java
@@ -0,0 +1,36 @@
+package main.java;
+
+public class RunningSumOfArray {
+
+    public static void main(String[] args) {
+
+        int[] result = runningSum(new int[]{1, 2, 3, 4});
+
+        for (int element : result) {
+
+            System.out.println(element);
+
+        }
+
+    }
+
+    public static int[] runningSum(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            sum += nums[i];
+
+            result[i] = sum;
+
+        }
+
+        return result;
+
+    }
+
+}
+
