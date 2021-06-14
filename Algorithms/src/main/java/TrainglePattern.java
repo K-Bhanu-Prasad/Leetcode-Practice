d3368a0676a4e43e76b4d058e96a69ae57329327
K-Bhanu-Prasad <83743790+K-Bhanu-Prasad@users.noreply.github.com>
2021-06-14 23:40:10 +0530

    Create TrainglePattern.java

---
+++ Algorithms/src/main/java/TrainglePattern.java
@@ -0,0 +1,27 @@
+package main.java;
+public class TrianglePattern {
+
+    /*  input       n = 3
+        output       1
+                    212
+                   32123
+     */
+    public static void main(String[] args) {
+        printPattern(3);
+    }
+
+    private static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1, k = n; j <= n + i - 1; j++) {
+                if (j < n - i + 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(k);
+                }
+                if (j >= n) k++;
+                else k--;
+            }
+            System.out.println();
+        }
+    }
+}
